49548645cbd518713a11ffe04121634598be1228
Ursula Moff <umoff@at-o6.21-school.ru>
2019-10-22 18:19:05 +0300

    1

--- Floats/ft_division_long_arithmetic.c
+++ Floats/ft_division_long_arithmetic.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 int *ft_make_zero_str(int size)
 {
     int *s;                                                                 // создаем строку
@@ -20,14 +19,65 @@ int *ft_make_zero_str(int size)
     return(s);                                                              // возварщаем int-ую нулевую строку
 }
 
-int    *ft_addition_long_arithmetic(int *x, int *y)
+int	ft_array_comparison(int *my_array)
 {
-    int *result;                                                            // результат сложения
+    int i;
+    int count;
+
+    i = 0;
+    count = 0;
+    while(my_array[i] == 0)
+        i++;
+    while (i < 1100)
+    {
+        i++;
+        count++;
+    }
+    return (count);
+}
+
+int *ft_shift_elements(int *my_array, int len)
+{
     int i;
 
-    i = 1100 - 1;                                                       // начинаем с элемента = длина результата - 1 (последний символ для \0)
-    result = ft_make_zero_str(1100);                                    // создание нулевой int-овой строки
+    i = 0;
+    while(my_array[i] == 0)
+        i++;
+    while (i < 1100)
+    {
+        my_array[i - len] = my_array[i];
+        my_array[i] = 0;
+        i++;
+    }
+    return(my_array);
+}
 
+int ft_abs(int digital)
+{
+    if (digital < 0)
+        digital = (-1) * digital;
+    return (digital);
+}
+
+int    *ft_division_long_arithmetic(int *x, int *y)
+{
+    int *result;                                                            // результат сложения
+    int *temp;
+    int i;
+    int len;
+    int len_x;
+    int len_y;
+
+    i = 1100 - 1;                                                           // начинаем с элемента = длина результата - 1 (последний символ для \0)
+    result = ft_make_zero_str(1100);                                    // создание нулевой int-овой строки
+    len_x = ft_array_comparison(x);
+    len_y = ft_array_comparison(y);
+    len = ft_abs(len_x - len_y);
+    temp = ft_shift_elements(x, len); //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Здесь надо выбрать наименьшее число и выровнять с большим 
+    while(i >= 0)
+    {
+        printf("good");
+    }
     return(result);                                                         // выводим результат сложения
 }
 
@@ -45,7 +95,7 @@ int main()
     b = ft_make_zero_str(1100);
     a[1099] = 1;                                                            // 1 число из него вычитают
     a[1098] = 0;
-    a[1097] = 0;
+    a[1097] = 1;
     a[1096] = 0;
     a[1095] = 0;
 
@@ -57,7 +107,7 @@ int main()
     b[1095] = 0;
 
     c = ft_division_long_arithmetic (a, b);                               // запуск функции вычитания длинной арифметики
-    /* printf("%d", c[1092]); */
+
     while(c[i] == 0 && i < 1100)
         i++;
     if (i == 1100)
